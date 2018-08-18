072e92149a206d13d9d39308c0d7a7dde8643b3b
hao117 <beetle082@163.com>
2018-08-18 11:16:25 +0800

    增加log4j，log4j2，logback日志输出demo

---
+++ bee-apm-agent-test/bee-apm-test-web/src/main/java/net/beeapm/demo/controller/LoggerTestServlet.java
@@ -0,0 +1,78 @@
+package net.beeapm.demo.controller;
+
+
+import ch.qos.logback.classic.LoggerContext;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.slf4j.impl.StaticLoggerBinder;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Created by yuan on 2018/8/2.
+ */
+@WebServlet(urlPatterns = {"/logger"})
+public class LoggerTestServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        String context = request.getParameter("context");
+        System.out.println("=================================>DbTestServlet");
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            testLog4j();
+            testLog4j2();
+            testLogback();
+            PrintWriter writer = response.getWriter();
+            writer.append("Good!");
+            writer.flush();
+            writer.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        this.doGet(request, response);
+    }
+
+    private void testLog4j(){
+        BasicConfigurator.configure();
+        Logger logger = LogManager.getLogger(LoggerTestServlet.class);
+        logger.setLevel(Level.ALL);
+        logger.trace("Log4j==================>trace");
+        logger.debug("Log4j==================>debug");
+        logger.info("Log4j==================>info");
+        logger.warn("Log4j==================>warn");
+        logger.error("Log4j==================>error");
+        logger.fatal("Log4j==================>fatal");
+    }
+
+    private void testLog4j2(){
+        System.setProperty("log4j2.debug","debug");
+        org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger(LoggerTestServlet.class);
+        logger.trace("Log4j2==================>trace");
+        logger.debug("Log4j2==================>debug");
+        logger.info("Log4j2==================>info");
+        logger.warn("Log4j2==================>warn");
+        logger.error("Log4j2==================>error");
+        logger.fatal("Log4j2==================>fatal");
+    }
+
+    private void testLogback(){
+        LoggerContext loggerFactory = (LoggerContext)StaticLoggerBinder.getSingleton().getLoggerFactory();
+        ch.qos.logback.classic.Logger logger = loggerFactory.getLogger(LoggerTestServlet.class);
+        logger.setLevel(ch.qos.logback.classic.Level.ALL);
+        logger.trace("Logback==================>trace");
+        logger.debug("Logback==================>debug");
+        logger.info("Logback==================>info");
+        logger.warn("Logback==================>warn");
+        logger.error("Logback==================>error");
+    }
+}
