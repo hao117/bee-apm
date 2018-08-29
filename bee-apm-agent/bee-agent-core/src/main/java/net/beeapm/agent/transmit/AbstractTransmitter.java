b689e1903c0965c98ae7bf2cd7ee910d433c5670
hao117 <beetle082@163.com>
2018-08-29 22:23:58 +0800

    修改抽象类定义

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/transmit/AbstractTransmitter.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/transmit/AbstractTransmitter.java
@@ -3,11 +3,14 @@ package net.beeapm.agent.transmit;
 
 import net.beeapm.agent.model.Span;
 
+import java.util.List;
+
 /**
  * Created by yuan on 2018/8/5.
  */
 public abstract class AbstractTransmitter {
     private String name;
     public abstract int transmit(Span span);
+    public abstract int transmit(List<Span> span);
     public abstract int init();
 }
--- bee-apm-agent/bee-agent-transmitter/elasticsearch-transmitter/src/main/java/net/beeapm/agent/transmit/elasticsearch/ElasticSearchTransmitter.java
+++ bee-apm-agent/bee-agent-transmitter/elasticsearch-transmitter/src/main/java/net/beeapm/agent/transmit/elasticsearch/ElasticSearchTransmitter.java
@@ -3,9 +3,16 @@ package net.beeapm.agent.transmit.elasticsearch;
 import net.beeapm.agent.model.Span;
 import net.beeapm.agent.transmit.AbstractTransmitter;
 
+import java.util.List;
+
 public class ElasticSearchTransmitter extends AbstractTransmitter {
     @Override
     public int transmit(Span span) {
+        return 0;
+    }
+
+    @Override
+    public int transmit(List<Span> span) {
         return 0;
     }
 
