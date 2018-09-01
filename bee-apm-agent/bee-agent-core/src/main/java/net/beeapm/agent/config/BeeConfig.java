9bccc749f37c20385d072c5078f25bc2a4e5b117
hao117 <beetle082@163.com>
2018-09-01 23:17:29 +0800

    agent参数采集

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/config/BeeConfig.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/config/BeeConfig.java
@@ -2,12 +2,20 @@ package net.beeapm.agent.config;
 
 import net.beeapm.agent.log.LogLevel;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
  * 以后增加配置动态加载
  */
 public class BeeConfig {
     private static LogLevel logLevel;
     private static Boolean  isLogConsole;
+    private static Boolean enableParam;
+    private static Set<String> excludeParamTypes = new HashSet<String>();
+    private static Set<String> excludeParamTypePrefix = new HashSet<String>();
     public static void initConfig(){
         String level = ConfigUtils.me().getStr("logger.level","error");
         if(level.equalsIgnoreCase(LogLevel.TRACE.name())){
@@ -24,6 +32,18 @@ public class BeeConfig {
             logLevel = LogLevel.OFF;
         }
         isLogConsole = ConfigUtils.me().getBoolean("logger.console",false);
+        enableParam = ConfigUtils.me().getBoolean("plugins.process.enableParam",true);
+        List<String> excludeParamTypesList = ConfigUtils.me().getList("plugins.process.excludeParamTypes");
+        List<String> excludeParamTypePrefixList = ConfigUtils.me().getList("plugins.process.excludeParamTypePrefix");
+        excludeParamTypes.clear();
+        excludeParamTypePrefix.clear();
+        if(excludeParamTypesList != null && !excludeParamTypesList.isEmpty()){
+            excludeParamTypes.addAll(excludeParamTypesList);
+        }
+        if(excludeParamTypePrefixList != null && !excludeParamTypePrefixList.isEmpty()){
+            excludeParamTypePrefix.addAll(excludeParamTypePrefixList);
+        }
+
     }
 
     public static LogLevel getLogLevel(){
@@ -33,4 +53,28 @@ public class BeeConfig {
     public static boolean isLogConsole(){
         return isLogConsole;
     }
+
+    public static boolean isEnableParam(){
+        return enableParam;
+    }
+
+    public static Set<String> getExcludeParamTypes(){
+        return  excludeParamTypes;
+    }
+
+    public static boolean isExcludeParamType(Class clazz){
+        String name = clazz.getName();
+        if(excludeParamTypes.contains(name)){
+            return true;
+        }
+        Iterator<String> it = excludeParamTypePrefix.iterator();
+        while (it.hasNext()){
+            String prefix = it.next();
+            if(name.startsWith(prefix)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
