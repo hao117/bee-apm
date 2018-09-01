4e8814130849c2e909d1a2f8f025ca1415461f1a
hao117 <beetle082@163.com>
2018-09-01 23:01:28 +0800

    增加方法入参

--- bee-apm-agent-test/bee-apm-test-web/src/main/java/net/beeapm/demo/service/HelloServiceImpl.java
+++ bee-apm-agent-test/bee-apm-test-web/src/main/java/net/beeapm/demo/service/HelloServiceImpl.java
@@ -1,33 +1,54 @@
 package net.beeapm.demo.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by yuan on 2018/8/2.
  */
 public class HelloServiceImpl implements IHelloService {
     @Override
     public String sayHello(String context) {
-        System.out.println("     ============HelloServiceImpl.sayHello:"+context);
-        testA();
-        testB();
+        try {
+            System.out.println("     ============HelloServiceImpl.sayHello:" + context);
+            testA("a", "b");
+            testB(new FileInputStream(new File("C:\\Users\\yuan\\Desktop\\service.txt")));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return context;
     }
 
-    public void testA(){
-        testA1();
-        testA2();
+    public void testA(String a,String b){
+        testA1(12311L,"str111");
+        List list = new ArrayList();
+        list.add("list123");
+        testA2(list);
     }
 
-    public void testA1(){
+    public void testA1(Long num,String str){
 
     }
 
-    public void testA2(){
+    public void testA2(List list){
 
     }
 
-    public void testB(){
-        testB1();
-        testB2();
+    public void testB(FileInputStream fis){
+        try{
+            fis.close();
+            testB1();
+            Map<String,Object> p = new HashMap<>();
+            p.put("bbb1","bbb1");
+            p.put("bbb2","bbb");
+            testB2(p);
+        }catch (Exception e){
+
+        }
     }
 
     public void testB1(){
@@ -37,7 +58,7 @@ public class HelloServiceImpl implements IHelloService {
     public void testB1_1(){
 
     }
-    public void testB2(){
+    public void testB2(Map<String,Object> p){
 
     }
 }
