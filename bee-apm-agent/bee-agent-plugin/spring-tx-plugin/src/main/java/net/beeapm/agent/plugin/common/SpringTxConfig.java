e37780dad92ec51fb201ac8bcd6b2861ec7a1bd4
hao117 <beetle082@163.com>
2018-09-22 17:55:46 +0800

    spring事务采集重构

---
+++ bee-apm-agent/bee-agent-plugin/spring-tx-plugin/src/main/java/net/beeapm/agent/plugin/common/SpringTxConfig.java
@@ -0,0 +1,41 @@
+package net.beeapm.agent.plugin.common;
+
+import net.beeapm.agent.config.AbstractBeeConfig;
+import net.beeapm.agent.config.BeeConfigFactory;
+import net.beeapm.agent.config.ConfigUtils;
+
+public class SpringTxConfig extends AbstractBeeConfig {
+    private static SpringTxConfig config;
+    private Boolean enable;
+    private long spend;
+
+    public static SpringTxConfig me(){
+        if(config == null){
+            synchronized (SpringTxConfig.class){
+                if(config == null){
+                    config = new SpringTxConfig();
+                    BeeConfigFactory.me().registryConfig("springTx",config);
+                }
+            }
+        }
+        return config;
+    }
+
+    private SpringTxConfig(){
+        initConfig();
+    }
+
+    @Override
+    public void initConfig() {
+        enable = ConfigUtils.me().getBoolean("plugins.springTx.enable",true);
+        spend = ConfigUtils.me().getInt("plugins.springTx.spend",-1);
+    }
+
+    public long getSpend() {
+        return spend;
+    }
+
+    public Boolean isEnable() {
+        return enable;
+    }
+}
