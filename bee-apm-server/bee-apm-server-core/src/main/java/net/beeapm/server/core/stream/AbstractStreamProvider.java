dcadfc33f667a3a54794d77a619ffa69d49f5e57
hao117 <beetle082@163.com>
2018-11-03 16:52:24 +0800

    增加写数据到流程中

--- bee-apm-server/bee-apm-server-core/src/main/java/net/beeapm/server/core/stream/AbstractStreamProvider.java
+++ bee-apm-server/bee-apm-server-core/src/main/java/net/beeapm/server/core/stream/AbstractStreamProvider.java
@@ -1,4 +1,19 @@
 package net.beeapm.server.core.stream;
 
+import net.beeapm.server.core.common.Stream;
+import net.beeapm.server.core.handler.HandlerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class AbstractStreamProvider implements IStreamProvider {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractStreamProvider.class);
+    public String write(Stream stream){
+        try {
+            HandlerFactory.getInstance().executeFirstHandler(stream);
+            return "ok";
+        }catch (Exception e){
+            logger.error("",e);
+        }
+        return "fail";
+    }
 }
