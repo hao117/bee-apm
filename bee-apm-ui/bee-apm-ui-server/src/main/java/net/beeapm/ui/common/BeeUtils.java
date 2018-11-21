abf54fe87d8b19cdea7c10e531111d1f2ed928fa
hao117 <beetle082@163.com>
2018-11-21 21:02:42 +0800

    解决索引名称处理问题

--- bee-apm-ui/bee-apm-ui-server/src/main/java/net/beeapm/ui/common/BeeUtils.java
+++ bee-apm-ui/bee-apm-ui-server/src/main/java/net/beeapm/ui/common/BeeUtils.java
@@ -1,10 +1,15 @@
 package net.beeapm.ui.common;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public class BeeUtils {
+    private static String DATE_FORMAT_DAY = "yyyy-MM-dd";
     public static Long getBeginTime(Map<String,Object> param){
         String beginTime = (String) param.get("beginTime");
         try {
@@ -22,14 +27,33 @@ public class BeeUtils {
         return 0L;
     }
 
-    public static String[] getIndices(String index,Map<String,Object> param){
+    public static String[] getIndices(String indexPrefix,Map<String,Object> param){
         String beginTime = (String) param.get("beginTime");
         beginTime = beginTime.substring(0,10).replace("-",".");
         String endTime = (String) param.get("endTime");
         endTime = endTime.substring(0,10).replace("-",".");
         if(beginTime.equals(endTime)){
-            return new String[]{index+beginTime};
+            return new String[]{indexPrefix+beginTime};
         }
-        return new String[]{index+beginTime,index+endTime};
+        try {
+            Date begin = DateUtils.parseDate(beginTime, DATE_FORMAT_DAY);
+            Date end = DateUtils.parseDate(endTime, DATE_FORMAT_DAY);
+            if(begin.getTime() > end.getTime()){
+                throw new RuntimeException("开始时间不能大于结束时间,开始时间为" + begin + "结束时间为" + end);
+            }
+            List<String> indexList = new ArrayList<>();
+            indexList.add(indexPrefix+ DateFormatUtils.format(begin,DATE_FORMAT_DAY));
+            for(;;){
+                begin = DateUtils.addDays(begin,1);
+                if(begin.getTime() <= end.getTime()){
+                    indexList.add(indexPrefix+ DateFormatUtils.format(begin,DATE_FORMAT_DAY));
+                }else {
+                    break;
+                }
+            }
+            return indexList.toArray(new String[indexList.size()]);
+        }catch (Exception e){
+            throw new RuntimeException("日期格式不正确",e);
+        }
     }
 }
