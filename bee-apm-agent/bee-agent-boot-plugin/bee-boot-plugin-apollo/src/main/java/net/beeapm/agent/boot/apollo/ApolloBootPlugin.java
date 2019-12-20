1b196a6d23d3f81d070a5b243ff4536fbe3ef38e
hao117 <beetle082@163.com>
2019-12-20 19:59:07 +0800

    启动插件命名调整，插件配置改为注解

--- bee-apm-agent/bee-agent-boot-plugin/bee-boot-plugin-apollo/src/main/java/net/beeapm/agent/boot/apollo/ApolloBootPlugin.java
+++ bee-apm-agent/bee-agent-boot-plugin/bee-boot-plugin-apollo/src/main/java/net/beeapm/agent/boot/apollo/ApolloBootPlugin.java
@@ -2,56 +2,60 @@ package net.beeapm.agent.boot.apollo;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import net.beeapm.agent.annotation.BeePlugin;
+import net.beeapm.agent.annotation.BeePluginType;
 import net.beeapm.agent.boot.AbstractBootPlugin;
 import net.beeapm.agent.common.BeeAgentJarUtils;
 import net.beeapm.agent.common.SysPropKey;
+import net.beeapm.agent.log.BeeLog;
 
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 
 /**
- * Created by yuan on 2018/11/6.
+ * @author yuan
+ * @date 2018/11/06
  */
-public class ApolloBootPlugin extends AbstractBootPlugin{
+@BeePlugin(type = BeePluginType.BOOT, name = "apollo")
+public class ApolloBootPlugin extends AbstractBootPlugin {
     @Override
     public void boot() {
-        System.out.println("=============>ApolloBootPlugin start ..................");
+        BeeLog.write("=============>ApolloBootPlugin start ..................");
         try {
             //{config_server_url}/configs/{appId}/{clusterName}/{namespaceName}?releaseKey={releaseKey}&ip={clientIp}
             String namespaceName = "config.yml";
             String configPath = System.getProperty(SysPropKey.BEE_CONFIG);
-            if(configPath == null){
+            if (configPath == null) {
                 configPath = BeeAgentJarUtils.getAgentJarDirPath() + "/" + namespaceName;
             }
             String env = System.getProperty("env");
-            if(env == null){
+            if (env == null) {
                 return;
             }
-            String url = System.getProperty(env.toLowerCase()+"_meta");
+            String url = System.getProperty(env.toLowerCase() + "_meta");
             String appId = System.getProperty("app.id");
             String clusterName = System.getProperty("apollo.cluster");
-            if(clusterName == null){
+            if (clusterName == null) {
                 clusterName = "default";
             }
 
-            if(url == null || url.isEmpty()){
-                System.out.println("=============>apollo插件启动失败，没有配置apollo地址，不拉取配置。如果不需要从apollo拉取配置，请忽略改提示！");
+            if (url == null || url.isEmpty()) {
+                BeeLog.log("=============>apollo插件启动失败，没有配置apollo地址，不拉取配置。如果不需要从apollo拉取配置，请忽略改提示！");
                 return;
             }
 
             String ip = System.getProperty(SysPropKey.BEE_IP);
             url = url + "/configs/" + appId + "/" + clusterName + "/" + namespaceName;
             System.out.println(url);
-            String result = sendHttpGet(url,ip);
-            if(result != null && !result.isEmpty()){
+            String result = sendHttpGet(url, ip);
+            if (result != null && !result.isEmpty()) {
                 System.out.println(result);
                 JSONObject json = JSON.parseObject(result);
                 String content = json.getJSONObject("configurations").getString("content");
-                //String releaseKey = json.getString("releaseKey");
-                writeFile(configPath,content);
+                writeFile(configPath, content);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -60,7 +64,7 @@ public class ApolloBootPlugin extends AbstractBootPlugin{
         String result = "";
         BufferedReader in = null;
         try {
-            if(param != null){
+            if (param != null) {
                 url = url + "?" + param;
             }
             URL realUrl = new URL(url);
@@ -70,14 +74,14 @@ public class ApolloBootPlugin extends AbstractBootPlugin{
             connection.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.9");
             connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
             connection.connect();
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream(),"UTF-8"));
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 if (in != null) {
                     in.close();
@@ -89,26 +93,26 @@ public class ApolloBootPlugin extends AbstractBootPlugin{
         return result;
     }
 
-    private void writeFile(String path,String content){
-        if(content == null){
+    private void writeFile(String path, String content) {
+        if (content == null) {
             return;
         }
         FileWriter fileWriter = null;
-        try{
-            File file =new File(path);
-            if(!file.exists()){
+        try {
+            File file = new File(path);
+            if (!file.exists()) {
                 file.createNewFile();
             }
             fileWriter = new FileWriter(file.getCanonicalPath(), false);
             fileWriter.write(content.toCharArray());
             fileWriter.flush();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(fileWriter != null){
+        } finally {
+            if (fileWriter != null) {
                 try {
                     fileWriter.close();
-                }catch (Exception e){
+                } catch (Exception e) {
                 }
             }
         }
