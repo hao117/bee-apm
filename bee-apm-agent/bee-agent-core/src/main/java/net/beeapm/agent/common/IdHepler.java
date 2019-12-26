6a77846bb93e3f6ca0600649bc420ce0626db8ce
hao117 <beetle082@163.com>
2019-12-26 19:55:12 +0800

    日志打印调整，定时任务调整

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/IdHepler.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/IdHepler.java
@@ -1,6 +1,10 @@
 package net.beeapm.agent.common;
 
+import net.beeapm.agent.config.BeeConfig;
 import net.beeapm.agent.config.ConfigUtils;
+import net.beeapm.agent.log.BeeLog;
+import net.beeapm.agent.log.LogImpl;
+import net.beeapm.agent.log.LogManager;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.state.ConnectionState;
@@ -8,57 +12,76 @@ import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * Created by yuan on 2018/8/4.
+ * @author yuan
+ * @date 2018/08/04
  */
 public class IdHepler {
     private static boolean initFlag = false;
     private static char[] lock = new char[1];
     private static CuratorFramework client = null;
     public static String nodeName;
-    private static String datePattern = "yyMMddHHmmss";
+    private static String datePattern = "yyyy-MM-dd HH:mm:ss";
     private static SimpleDateFormat sdf = new SimpleDateFormat(datePattern);
     public static AtomicLong id = new AtomicLong(1);
     public static String timeTag = sdf.format(new Date());
     public static String prevTimeTag = timeTag;
     public static AtomicInteger initTimes = new AtomicInteger(0);
     private static String rootDir = "/bee/ids/";
+    private static final LogImpl log = LogManager.getLog(IdHepler.class.getSimpleName());
+    private static ScheduledExecutorService service;
+
+    static {
+        service = new ScheduledThreadPoolExecutor(1, new ThreadFactory() {
+            private final AtomicLong mThreadNum = new AtomicLong(1);
+
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "bee-id-thread-" + mThreadNum.getAndIncrement());
+            }
+        });
+    }
 
     public static void init() {
         synchronized (lock) {
-            if(client == null) {
+            if (client == null) {
                 try {
-                    System.out.println("init zk 。。。。。。。。。。。。。。。。。。。。。。");
+                    log.info("init zk 。。。。。。。。。。。。。。。。。。。。。。");
                     String server = ConfigUtils.me().getStr("id.zk.url");
-                    int timeout = ConfigUtils.me().getInt("id.zk.timeout", 5000);//秒
+                    log.info("zk address = {}", server);
+                    BeeLog.log("zk address = " + server);
+                    int timeout = ConfigUtils.me().getInt("id.zk.timeout", 5000);
                     int times = ConfigUtils.me().getInt("id.zk.retryTimes", 2);
-                    int sleep = ConfigUtils.me().getInt("id.zk.retrySleep", 5000);//毫秒
+                    int sleep = ConfigUtils.me().getInt("id.zk.retrySleep", 5000);
                     System.out.println("zk address = " + server);
                     if (server == null) {
                         return;
                     }
                     client = CuratorFrameworkFactory.builder()
                             .connectString(server)
-                            .sessionTimeoutMs(1000 * 10)
+                            .sessionTimeoutMs(timeout)
                             .connectionTimeoutMs(timeout)
                             .retryPolicy(new RetryNTimes(times, sleep))
                             .build();
                     client.getConnectionStateListenable().addListener(clientListener);
                     client.start();
-                    System.out.println("bee apm agent init node name");
+                    log.info("bee apm agent init node name");
                     genNodeName(initTimes.incrementAndGet());
-                    System.out.println("bee apm init time tag");
+                    log.info("bee apm init time tag");
                     initTag();
                     initFlag = true;
-                }catch (Throwable e){
+                } catch (Throwable e) {
                     e.printStackTrace();
                     System.exit(0);
                 }
@@ -66,126 +89,117 @@ public class IdHepler {
         }
     }
 
-    private static void initTag(){
+    private static void initTag() {
         Calendar cal = Calendar.getInstance();
         timeTag = sdf.format(cal.getTime());
         prevTimeTag = timeTag;
-        cal.set(Calendar.SECOND,cal.get(Calendar.SECOND)+1);
-        cal.set(Calendar.MILLISECOND,0);
+        cal.set(Calendar.SECOND, cal.get(Calendar.SECOND) + 1);
+        cal.set(Calendar.MILLISECOND, 0);
         long delay = cal.getTimeInMillis() - System.currentTimeMillis();
-        if(!initFlag){
-            Timer timer = new Timer(true);
-            timer.scheduleAtFixedRate(new TimerTask() {
+        if (!initFlag) {
+            service.scheduleAtFixedRate(new Runnable() {
                 @Override
                 public void run() {
                     timeTag = sdf.format(new Date());
                 }
-            },delay,1000);
+            }, delay, 1000, TimeUnit.MILLISECONDS);
+
         }
     }
 
-    public static String id(){
-        if(!initFlag) {
+    public static String id() {
+        if (!initFlag) {
             init();
         }
-        if(nodeName == null){
+        if (nodeName == null) {
             return null;
         }
-        if(!prevTimeTag.equals(timeTag)){
-            synchronized(IdHepler.class){
-                if(!prevTimeTag.equals(timeTag)) {
+        if (!prevTimeTag.equals(timeTag)) {
+            synchronized (IdHepler.class) {
+                if (!prevTimeTag.equals(timeTag)) {
                     prevTimeTag = timeTag;
                     id.set(1);
                 }
             }
         }
-        return nodeName+prevTimeTag+id.getAndIncrement();
+        return nodeName + prevTimeTag + id.getAndIncrement();
     }
 
 
-    private static String buildNodeText(int nodeName,int times){
+    private static String buildNodeText() {
         StringBuilder sb = new StringBuilder();
-        String buildDate = "bee ";
-//        try{
-//            buildDate = ManifestUtils.getManifest(IdHepler.class).get("Build-Date");
-//        }catch (Exception e){
-//        }
-        sb.append("startTime=").append(sdf.format(new Date()));
-//                .append(",times=").append(times)
-//                .append(",version=").append(HLogConfig.VERSION)
-//                .append(",verNum=").append(HLogConfig.VER_NUM)
-//                .append(",buildDate=").append(buildDate)
-//                .append(",serverGrp=").append(System.getProperty(Constants.SYS_KEY_HLOG_SERVER_GROUP))
-//                .append(",serverAlias=").append(System.getProperty(Constants.SYS_KEY_HLOG_SERVER_ALIAS))
-//                .append(",ip=").append(System.getProperty(Constants.SYS_KEY_HLOG_AGENT_IP))
-//                .append(",port=").append(System.getProperty("server.port"));
+        sb.append("startTime=").append(sdf.format(new Date()))
+                .append(",ip=").append(BeeConfig.me().getIp())
+                .append(",port=").append(BeeConfig.me().getPort())
+                .append(",app=").append(BeeConfig.me().getApp())
+                .append(",inst=").append(BeeConfig.me().getInst());
         return sb.toString();
     }
 
-    private static int createNode(int nNodeName,int times){
+    private static int createNode(int nNodeName, int times) {
         try {
             client.create()
                     .creatingParentsIfNeeded()
                     .withMode(CreateMode.EPHEMERAL)
-                    .forPath(rootDir+nNodeName,buildNodeText(nNodeName,times).getBytes("utf-8"));
-            //System.out.println("======> hlog createNode = " + str);
+                    .forPath(rootDir + nNodeName, buildNodeText().getBytes("utf-8"));
             return 1;
-        }catch (KeeperException.NodeExistsException e){
-            //System.out.println("NodeExistsException nodeName="+nNodeName);
+        } catch (KeeperException.NodeExistsException e) {
             return 0;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return -1;
     }
 
-    private static void genNodeName(int times){
+    private static void genNodeName(int times) {
         nodeName = null;
-        String s_start = ConfigUtils.me().getStr("id.start","1001");
-        String s_end = ConfigUtils.me().getStr("id.end","9999");
+        String s_start = ConfigUtils.me().getStr("id.start", "1001");
+        String s_end = ConfigUtils.me().getStr("id.end", "9999");
         int start = Integer.parseInt(s_start);
         int end = Integer.parseInt(s_end);
-        for(int i = start; i <= end; i++){
-            int flag = createNode(i,times);
-            if(flag == 1){
+        for (int i = start; i <= end; i++) {
+            int flag = createNode(i, times);
+            if (flag == 1) {
                 nodeName = i + "";
-                System.out.println("bee id flag nodeName = " + nodeName);
+                log.info("bee id flag nodeName = " + nodeName);
                 break;
-            }else if(flag == 0){
+            } else if (flag == 0) {
                 continue;
-            }else{
+            } else {
                 break;
             }
         }
     }
 
-    // 客户端的监听配置
+    /**
+     * 客户端的监听配置
+     */
     static ConnectionStateListener clientListener = new ConnectionStateListener() {
+        @Override
         public void stateChanged(CuratorFramework client, ConnectionState newState) {
             if (newState == ConnectionState.CONNECTED) {
-                //countDownLatch.countDown();
-                System.out.println("zk connected established");
+                log.error("zk connected established");
             } else if (newState == ConnectionState.LOST) {
                 try {
                     client.close();
-                }catch (Exception e){
+                } catch (Exception e) {
                 }
                 client = null;
                 try {
                     init();
                 } catch (Exception e) {
-                    System.err.println("re-init failed");
+                    log.error("re-init failed");
                 }
-            }else if(newState == ConnectionState.RECONNECTED){
+            } else if (newState == ConnectionState.RECONNECTED) {
                 try {
                     client.close();
-                }catch (Exception e){
+                } catch (Exception e) {
                 }
                 client = null;
                 try {
                     init();
                 } catch (Exception e) {
-                    System.err.println("re-init failed");
+                    log.error("re-init failed");
                 }
             }
         }
