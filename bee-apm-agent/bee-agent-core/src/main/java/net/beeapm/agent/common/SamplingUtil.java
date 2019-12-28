0427b8da06305344cbefe07b5d7d68ec54d68655
hao117 <beetle082@163.com>
2019-12-28 08:55:55 +0800

    添加注释

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/SamplingUtil.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/SamplingUtil.java
@@ -4,49 +4,55 @@ import net.beeapm.agent.config.BeeConfig;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * 采样率处理工具类
+ *
+ * @author yuanlong.chen
+ * @date 2018/09/26
+ */
 public class SamplingUtil {
     private static AtomicLong total = new AtomicLong(0);
     private static AtomicLong currNum = new AtomicLong(0);
 
-    public static long incrTotal(){
+    public static long incrTotal() {
         return total.incrementAndGet();
     }
 
-    public static long getTotal(){
+    public static long getTotal() {
         return total.get();
     }
 
-    public static long incrCurrNum(){
+    public static long incrCurrNum() {
         return currNum.incrementAndGet();
     }
 
-    public static long getCurrNum(){
+    public static long getCurrNum() {
         return currNum.get();
     }
 
-    public static boolean YES(){
+    public static boolean YES() {
         return isCollect();
     }
 
-    public static boolean NO(){
+    public static boolean NO() {
         return !isCollect();
     }
 
-    private static boolean isCollect(){
-        if(BeeConfig.me().getRate() <= 0){
+    private static boolean isCollect() {
+        if (BeeConfig.me().getRate() <= 0) {
             BeeTraceContext.setCTag("N");
             return false;
         }
-        if(BeeConfig.me().getRate() >= 10000){
+        if (BeeConfig.me().getRate() >= 10000) {
             BeeTraceContext.setCTag("Y");
             return true;
         }
         String ctag = BeeTraceContext.getCTag();
-        if("Y".equals(ctag)){
+        if ("Y".equals(ctag)) {
             return true;
-        }else if("N".equals(ctag)){
+        } else if ("N".equals(ctag)) {
             return false;
-        }else if(getCurrNum() == 0){//第一条采集
+        } else if (getCurrNum() == 0) {//第一条采集
             incrCurrNum();
             incrTotal();
             BeeTraceContext.setCTag("Y");
@@ -55,7 +61,7 @@ public class SamplingUtil {
         long tmpTotal = incrTotal();
         long tmpCurrNum = getCurrNum() + 1;
         Double rate = tmpCurrNum * 1.0 / tmpTotal * 10000;
-        if(rate.intValue() > BeeConfig.me().getRate()){
+        if (rate.intValue() > BeeConfig.me().getRate()) {
             BeeTraceContext.setCTag("N");
             return false;
         }
