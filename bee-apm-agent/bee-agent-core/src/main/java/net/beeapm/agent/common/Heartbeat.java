c59cad854ef02f2836d0539f7bea4678a2dbfe9f
hao117 <beetle082@163.com>
2020-01-11 22:18:55 +0800

    重构

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/Heartbeat.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/Heartbeat.java
@@ -8,18 +8,28 @@ import net.beeapm.agent.reporter.ReporterFactory;
 import java.util.concurrent.*;
 
 public class Heartbeat {
-    private static ScheduledExecutorService service = new ScheduledThreadPoolExecutor(1,new BeeThreadFactory("heartbeat"));
+    private static ScheduledExecutorService service = new ScheduledThreadPoolExecutor(1, new BeeThreadFactory("heartbeat"));
 
-    public static void start(){
+    public static String id;
+
+    public static void start() {
         int period = BeeConfig.me().getPeriod();
         service.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 Span span = new Span(SpanType.HEARTBEAT);
                 span.fillEnvInfo();
-                span.setId(IdHepler.id());
+                span.setId(getId());
                 ReporterFactory.report(span);
             }
-        },0,period, TimeUnit.SECONDS);
+        }, 0, period, TimeUnit.SECONDS);
+    }
+
+    private static String getId() {
+        if (id != null) {
+            return id;
+        }
+        id = BeeConfig.me().getEnv() + "_" + BeeConfig.me().getApp() + "_" + BeeConfig.me().getInst() + "_" + BeeConfig.me().getPort();
+        return id;
     }
 }
