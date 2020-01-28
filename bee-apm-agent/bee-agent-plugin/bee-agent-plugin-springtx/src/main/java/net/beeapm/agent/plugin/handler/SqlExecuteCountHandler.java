d02188bd1d6fb9e8631029dd99d6ba6d6ce893eb
hao117 <beetle082@163.com>
2020-01-28 14:13:08 +0800

    修复重复统计问题

--- bee-apm-agent/bee-agent-plugin/bee-agent-plugin-springtx/src/main/java/net/beeapm/agent/plugin/handler/SqlExecuteCountHandler.java
+++ bee-apm-agent/bee-agent-plugin/bee-agent-plugin-springtx/src/main/java/net/beeapm/agent/plugin/handler/SqlExecuteCountHandler.java
@@ -6,26 +6,42 @@ import net.beeapm.agent.log.LogManager;
 import net.beeapm.agent.model.Span;
 import net.beeapm.agent.plugin.common.SpringTxContext;
 
-
+/**
+ * 统计事务内sql执行次数
+ * @author yuan
+ * @date 2018-09-22
+ */
 public class SqlExecuteCountHandler extends AbstractHandler {
     private static final LogImpl log = LogManager.getLog(SqlExecuteCountHandler.class.getSimpleName());
     private static final String KEY_SQL_COUNT = "count";
+    private static final int VAL_STACK_DEPTH = 3;
+
     @Override
-    public Span before(String className, String methodName, Object[] allArguments,Object[] extVal) {
+    public Span before(String className, String methodName, Object[] allArguments, Object[] extVal) {
         Span span = SpringTxContext.getTxSpan();
-        if(span != null){
-           Integer sqlCount = (Integer)span.getTag(KEY_SQL_COUNT);
-           if(sqlCount == null){
-               sqlCount = new Integer(0);
-           }
-           sqlCount++;
-           span.addTag(KEY_SQL_COUNT,sqlCount);
+        if (span != null) {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            //排除父子关系调用
+            if (stackTrace != null && stackTrace.length > VAL_STACK_DEPTH) {
+                String method1 = stackTrace[2].getMethodName();
+                String method2 = stackTrace[3].getMethodName();
+                log.debug("method1={},method2={}", method1, method2);
+                if (method1.equals(method2)) {
+                    return span;
+                }
+            }
+            Integer sqlCount = (Integer) span.getTag(KEY_SQL_COUNT);
+            if (sqlCount == null) {
+                sqlCount = new Integer(0);
+            }
+            sqlCount++;
+            span.addTag(KEY_SQL_COUNT, sqlCount);
         }
         return span;
     }
 
     @Override
-    public Object after(String className, String methodName, Object[] allArguments, Object result, Throwable t,Object[] extVal) {
+    public Object after(String className, String methodName, Object[] allArguments, Object result, Throwable t, Object[] extVal) {
         return result;
     }
 
