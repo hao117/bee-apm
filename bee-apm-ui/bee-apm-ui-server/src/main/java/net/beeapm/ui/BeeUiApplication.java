30da4ec872df7f58d94af2792c253607236c054f
hao117 <beetle082@163.com>
2020-01-29 23:16:25 +0800

    ui端启动类类名修改

--- bee-apm-ui/bee-apm-ui-server/src/main/java/net/beeapm/ui/BeeUiApplication.java
+++ bee-apm-ui/bee-apm-ui-server/src/main/java/net/beeapm/ui/BeeUiApplication.java
@@ -5,17 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author yuan
+ * @date 2018-09-23
+ */
 @SpringBootApplication
-public class WebServerApplication extends SpringBootServletInitializer {
+public class BeeUiApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		application.initializers(new BeeInitializer());
-		return application.sources(WebServerApplication.class);
+		return application.sources(BeeUiApplication.class);
 	}
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication springApplication = new SpringApplication(WebServerApplication.class);
+		SpringApplication springApplication = new SpringApplication(BeeUiApplication.class);
 		springApplication.addInitializers(new BeeInitializer());
 		springApplication.run(args);
 	}
