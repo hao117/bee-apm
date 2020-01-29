f33561884ec64dcffabd8b71a1452f3f8ebd98c2
hao117 <beetle082@163.com>
2020-01-29 23:16:40 +0800

    server端启动类类名修改

--- bee-apm-server/bee-apm-server-web/src/main/java/net/beeapm/server/BeeServerApplication.java
+++ bee-apm-server/bee-apm-server-web/src/main/java/net/beeapm/server/BeeServerApplication.java
@@ -4,17 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+/**
+ * @author yuan
+ * @date 2018-08-24
+ */
 @SpringBootApplication
-public class BeeServerBoot extends SpringBootServletInitializer {
+public class BeeServerApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         application.initializers(new BeeInitializer());
-        return application.sources(BeeServerBoot.class);
+        return application.sources(BeeServerApplication.class);
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication springApplication = new SpringApplication(BeeServerBoot.class);
+        SpringApplication springApplication = new SpringApplication(BeeServerApplication.class);
         springApplication.addInitializers(new BeeInitializer());
         springApplication.run(args);
     }
