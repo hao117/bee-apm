c6161193c202592756339676754df3516649483f
hao117 <beetle082@163.com>
2020-05-07 22:29:47 +0800

    删除多余代码

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/plugin/handler/HandlerLoader.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/plugin/handler/HandlerLoader.java
@@ -8,8 +8,10 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Created by yuan on 2018/7/31.
+ * 插件handler加载
+ *
  * @author yuan
+ * @date 2018/7/31
  */
 public class HandlerLoader {
     private static final LogImpl log = LogManager.getLog(HandlerLoader.class.getSimpleName());
@@ -17,28 +19,29 @@ public class HandlerLoader {
     private static ConcurrentHashMap<String, Class<?>> classMap = new ConcurrentHashMap<String, Class<?>>();
     private static ReentrantLock INSTANCE_LOAD_LOCK = new ReentrantLock();
     private static AgentClassLoader beeClassLoader;
+    private static final EmptyHandler EMPTY_HANDLER = new EmptyHandler();
 
-    public static AgentClassLoader getBeeClassLoader(ClassLoader parentClassLoader){
-        if(beeClassLoader == null){
-            synchronized (HandlerLoader.class){
-                if(beeClassLoader == null){
-                    beeClassLoader = new AgentClassLoader(parentClassLoader,new String[]{"plugins"});
+    public static AgentClassLoader getBeeClassLoader(ClassLoader parentClassLoader) {
+        if (beeClassLoader == null) {
+            synchronized (HandlerLoader.class) {
+                if (beeClassLoader == null) {
+                    beeClassLoader = new AgentClassLoader(parentClassLoader, new String[]{"plugins"});
                 }
             }
         }
         return beeClassLoader;
     }
 
-    public static IHandler load(String className){
+    public static IHandler load(String className) {
         try {
-            ClassLoader contextClassLoader =  Thread.currentThread().getContextClassLoader();
+            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
             ClassLoader classLoader = getBeeClassLoader(contextClassLoader);
             String instanceKey = className + "_OF_" + classLoader.getClass().getName() + "@" + Integer.toHexString(classLoader.hashCode());
             IHandler inst = handlerMap.get(instanceKey);
             if (inst == null) {
                 INSTANCE_LOAD_LOCK.lock();
                 try {
-                    inst = (IHandler)Class.forName(className, true, classLoader).newInstance();
+                    inst = (IHandler) Class.forName(className, true, classLoader).newInstance();
                 } finally {
                     INSTANCE_LOAD_LOCK.unlock();
                 }
@@ -47,42 +50,15 @@ public class HandlerLoader {
                 }
             }
             return inst;
-        }catch (IllegalAccessException e){
-            log.error("",e);
-        }catch (InstantiationException e){
-            log.error("",e);
-        }catch (ClassNotFoundException e){
-            log.error("",e);
-        }catch (Throwable t){
-            log.error("",t);
+        } catch (IllegalAccessException e) {
+            log.error("plugin handler load error-IllegalAccessException", e);
+        } catch (InstantiationException e) {
+            log.error("plugin handler load error-InstantiationException", e);
+        } catch (ClassNotFoundException e) {
+            log.error("plugin handler load error-ClassNotFoundException", e);
+        } catch (Throwable t) {
+            log.error("plugin handler load error-Throwable", t);
         }
-        return new EmptyHandler();
-    }
-
-    public static Class<?> loadClass(String className){
-        log.debug("loadClass:"+className);
-        Class<?> clazz = null;
-        try {
-            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-            ClassLoader classLoader = getBeeClassLoader(contextClassLoader);
-            String clazzKey = className + "_OF_" + classLoader.getClass().getName() + "@" + Integer.toHexString(classLoader.hashCode());
-            clazz = classMap.get(clazzKey);
-            if (clazz == null) {
-                INSTANCE_LOAD_LOCK.lock();
-                try {
-                    clazz = Class.forName(className, true, classLoader);
-                } finally {
-                    INSTANCE_LOAD_LOCK.unlock();
-                }
-                if (clazz != null) {
-                    classMap.put(clazzKey, clazz);
-                }
-            }
-        }catch (ClassNotFoundException e){
-            log.error("",e);
-        }catch (Throwable t){
-            log.error("",t);
-        }
-        return clazz;
+        return EMPTY_HANDLER;
     }
 }
