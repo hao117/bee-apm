670f2676ee9f51e3e9035eddcf4556350222fbde
hao117 <beetle082@163.com>
2020-05-07 21:10:06 +0800

    添加注释

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/boot/AbstractBootPlugin.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/boot/AbstractBootPlugin.java
@@ -1,5 +1,9 @@
 package net.beeapm.agent.boot;
 
+/**
+ * @author yuan
+ * @date 2018-11-07
+ */
 public abstract class AbstractBootPlugin implements IBootPlugin{
     private String name;
     public void setName(String name){
