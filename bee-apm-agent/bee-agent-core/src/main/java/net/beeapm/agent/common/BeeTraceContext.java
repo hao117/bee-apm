e3c3bba3ffa468a33a73e3aff6a4ee35c6b203b7
hao117 <beetle082@163.com>
2020-05-08 22:22:32 +0800

    添加注释

--- bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/BeeTraceContext.java
+++ bee-apm-agent/bee-agent-core/src/main/java/net/beeapm/agent/common/BeeTraceContext.java
@@ -4,58 +4,67 @@ package net.beeapm.agent.common;
 import net.beeapm.agent.model.Span;
 
 /**
- * Created by yuan on 2018/8/4.
+ * 日志上下文
+ *
+ * @author yuan
+ * @date 2018/8/4
  */
 public class BeeTraceContext {
     private static final ThreadLocal<String> localGId = new ThreadLocal<String>();
     private static final ThreadLocal<String> localPId = new ThreadLocal<String>();
     private static final ThreadLocal<String> localCTag = new ThreadLocal<String>();
 
-    public static void clearAll(){
+    public static void clearAll() {
         localGId.remove();
         localPId.remove();
         localCTag.remove();
     }
 
-
-    public static String getPId(){
+    public static String getPId() {
         return localPId.get();
     }
 
-    public static void setPId(String pId){
+    public static void setPId(String pId) {
         localPId.set(pId);
     }
 
-    public static String getGId(){
+    public static String getGId() {
         String gid = localGId.get();
-        if(gid == null){
+        if (gid == null) {
             gid = IdHelper.id();
             setGId(gid);
         }
         return gid;
     }
 
-    public static void setGId(String gId){
+    public static void setGId(String gId) {
         localGId.set(gId);
     }
 
-    public static String getCTag(){
+    /**
+     * 采集标识：Y采集，N不采集
+     *
+     * @return
+     */
+    public static String getCTag() {
         return localCTag.get();
     }
 
-    public static void setCTag(String ctag){
+    /**
+     * 设置采集标识：Y采集，N不采集
+     *
+     * @param ctag
+     */
+    public static void setCTag(String ctag) {
         localCTag.set(ctag);
     }
 
 
-    public static String getCurrentId(){
+    public static String getCurrentId() {
         Span span = SpanManager.getCurrentSpan();
-        if(span == null){
+        if (span == null) {
             return "nvl";
         }
         return span.getId();
     }
-
-
-
 }
