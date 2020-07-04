3470bca91da57c526a5a61ded60dd71dd0571a54
hao117 <beetle082@163.com>
2020-07-04 23:08:16 +0800

    线程池拦截调整

--- bee-apm-agent/bee-agent-plugin/bee-agent-plugin-thread/src/main/java/net/beeapm/agent/plugin/thread/ThreadPlugin.java
+++ bee-apm-agent/bee-agent-plugin/bee-agent-plugin-thread/src/main/java/net/beeapm/agent/plugin/thread/ThreadPlugin.java
@@ -11,9 +11,8 @@ import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinTask;
+
 
 /**
  * @author yuan
@@ -32,21 +31,23 @@ public class ThreadPlugin extends AbstractPlugin {
                 new InterceptPoint() {
                     @Override
                     public ElementMatcher<TypeDescription> buildTypesMatcher() {
-                        return ElementMatchers.not(ElementMatchers.isInterface())
-                                .and(ElementMatchers.isSubTypeOf(ExecutorService.class)
-                                        .or(ElementMatchers.isSubTypeOf(CompletionService.class)))
+                        return ElementMatchers.any()
+                                .and(ElementMatchers.hasSuperType(ElementMatchers.named("java.util.concurrent.ExecutorService"))
+                                        .or(ElementMatchers.hasSuperType(ElementMatchers.named("java.util.concurrent.CompletionService"))))
+                                .and(ElementMatchers.not(ElementMatchers.isInterface()))
+                                .and(ElementMatchers.not(ElementMatchers.isAbstract()))
                                 .and(ElementMatchers.not(ElementMatchers.nameStartsWith("org.apache.tomcat.")));
                     }
 
                     @Override
                     public ElementMatcher<MethodDescription> buildMethodsMatcher() {
                         return ElementMatchers.isMethod()
-                                .and(ElementMatchers.<MethodDescription>named("submit")
-                                        .or(ElementMatchers.<MethodDescription>named("execute"))
-                                        .or(ElementMatchers.<MethodDescription>named("schedule"))
-                                        .or(ElementMatchers.<MethodDescription>named("scheduleAtFixedRate"))
-                                        .or(ElementMatchers.<MethodDescription>named("scheduleWithFixedDelay"))
-                                        .or(ElementMatchers.<MethodDescription>named("invoke")))
+                                .and(ElementMatchers.named("submit")
+                                        .or(ElementMatchers.named("execute"))
+                                        .or(ElementMatchers.named("schedule"))
+                                        .or(ElementMatchers.named("scheduleAtFixedRate"))
+                                        .or(ElementMatchers.named("scheduleWithFixedDelay"))
+                                        .or(ElementMatchers.named("invoke")))
                                 .and(ElementMatchers.takesArgument(0, Runnable.class)
                                         .or(ElementMatchers.takesArgument(0, Callable.class))
                                         .or(ElementMatchers.takesArgument(0, ForkJoinTask.class))
--- bee-apm-agent/bee-agent-plugin/bee-agent-plugin-thread/src/main/java/net/beeapm/agent/plugin/thread/handler/ThreadHandler.java
+++ bee-apm-agent/bee-agent-plugin/bee-agent-plugin-thread/src/main/java/net/beeapm/agent/plugin/thread/handler/ThreadHandler.java
@@ -1,7 +1,10 @@
 package net.beeapm.agent.plugin.thread.handler;
 
 import net.beeapm.agent.common.BeeTraceContext;
+import net.beeapm.agent.log.ILog;
+import net.beeapm.agent.log.LogFactory;
 import net.beeapm.agent.model.Span;
+import net.beeapm.agent.model.TraceContextModel;
 import net.beeapm.agent.plugin.handler.AbstractHandler;
 import net.beeapm.agent.plugin.thread.common.ThreadConst;
 import net.beeapm.agent.plugin.thread.wrapper.BeeCallableWrapper;
@@ -16,21 +19,27 @@ import java.util.concurrent.ForkJoinTask;
  * @date 2018/08/05
  */
 public class ThreadHandler extends AbstractHandler {
-
+    private static final ILog log = LogFactory.getLog(ThreadHandler.class.getSimpleName());
     @Override
     public Span before(String className, String methodName, Object[] allArguments, Object[] extVal) {
+        log.debug(className);
         Object task = allArguments[0];
-        Span span = new Span("t");
-        if (task instanceof BeeRunnableWrapper || task instanceof BeeCallableWrapper || task instanceof BeeForkJoinTaskWrapper) {
+
+        if (task instanceof BeeRunnableWrapper
+                || task instanceof BeeCallableWrapper
+                || task instanceof BeeForkJoinTaskWrapper) {
             return null;
         }
+        Span span = new Span("t");
         //修改入参
+        TraceContextModel traceContextModel = BeeTraceContext.getOrNew().copy();
+        traceContextModel.setPid(BeeTraceContext.getCurrentId());
         if (task instanceof Runnable) {
-            task = new BeeRunnableWrapper((Runnable) task, BeeTraceContext.getOrNew().copy());
+            task = new BeeRunnableWrapper((Runnable) task, traceContextModel);
         } else if (task instanceof Callable) {
-            task = new BeeCallableWrapper((Callable) task, BeeTraceContext.getOrNew().copy());
+            task = new BeeCallableWrapper((Callable) task, traceContextModel);
         } else if (task instanceof BeeForkJoinTaskWrapper) {
-            task = new BeeForkJoinTaskWrapper((ForkJoinTask) task, BeeTraceContext.getOrNew().copy());
+            task = new BeeForkJoinTaskWrapper((ForkJoinTask) task, traceContextModel);
         }
         span.addTag(ThreadConst.KEY_TASK, task);
         return span;
@@ -38,7 +47,6 @@ public class ThreadHandler extends AbstractHandler {
 
     @Override
     public Object after(String className, String methodName, Object[] allArguments, Object result, Throwable t, Object[] extVal) {
-
         return null;
     }
 
